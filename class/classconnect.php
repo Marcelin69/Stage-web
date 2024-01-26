ebcf6e0d3bc7c022127a0edccde3a027b75d0b15
Marcelin TINGOUGOUI <marcelintingougoui3@gmail.com>
2024-01-26 15:41:43 +0100

    Switch database connection from MySQL to PostgreSQL.

--- class/classconnect.php
+++ class/classconnect.php
@@ -3,7 +3,7 @@
 class Stage{
 public $satge;
 function __construct(){
-  $this->stage=new PDO('mysql:host=localhost;dbname=stagep;charset=utf8','root','');
+  $this->stage=new PDO('postgres://stage_ha36_user:wFR2reZtg3Er1nagwk3AJXwaLag9gHpa@dpg-cmps32g21fec73cjhjk0-a/stage_ha36');
 }
 
 function fetchAll($query){
